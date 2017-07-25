8fc716ec9583d85e510db6da43a2243a102c6247
maryana <maryana@maryanas-MacBook-Pro.local>
2017-07-25 16:20:31 -0400

    Finished Routing and Navigating Pages Module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule} from '@angular/core'
 import { BrowserModule} from '@angular/platform-browser'
 import { RouterModule} from '@angular/router'
 
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouterActivator,
+    EventListResolver
+
+} from './events/index'
 import { EventsAppComponent} from './events-app.component'
-import { EventsListComponent} from './events/events-list.component'
-import { EventThumbnailComponent} from './events/event-thumbnail.component'
 import { NavBarComponent} from './nav/navbar.component'
-import { EventService} from './events/shared/event.service'
 import { ToastrService} from './common/toastr.service'
-import { EventDetailsComponent} from './events/event-details/event-details.component'
-import { CreateEventComponent} from './events/create-event.component'
 import { Error404Component} from './errors/404.component'
 import { appRoutes} from './routes'
-import { EventRouterActivator} from './events/event-details/event-router-activator.service'
 
 
 @NgModule({
@@ -33,7 +37,8 @@ import { EventRouterActivator} from './events/event-details/event-router-activat
         EventService, 
         ToastrService, 
         EventRouterActivator,
-        {provide: 'canDeactivateCreateEvent', useValue: checkDirtyState}
+        {provide: 'canDeactivateCreateEvent', useValue: checkDirtyState},
+        EventListResolver
         ],
     bootstrap: [EventsAppComponent]
 })
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -21,7 +21,7 @@ import { ActivatedRoute } from '@angular/router'
 export class EventsListComponent implements OnInit {
     events: any
     constructor (private eventService: EventService, private toastr: ToastrService,
-    private route: ActivatedRoute){
+        private route: ActivatedRoute){
         
     }
 
